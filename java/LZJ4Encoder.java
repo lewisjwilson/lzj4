9256a04217c6a824a01abb7d8499590ae3f0dddb
Thicksandwich Development <lewis.j.wilson@outlook.com>
2022-08-20 23:54:32 +0900

    Started to create LZ4 Data blocks

--- java/LZJ4Encoder.java
+++ java/LZJ4Encoder.java
@@ -1,30 +1,43 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class LZJ4Encoder {
-    
+
     // Data variables
     static String testData = "abbccabbcccabbaabcc";
     static int dataLen = testData.length();
 
-    // Search buffer variables
-    static int windowBuf = dataLen; // temporarily - to encode all data at once
-    static int pos = 0;
+    // Window buffer and search buffer variables
+    static int windowBuf = 100000; // temporarily - to encode all data at once
+    static int searchBuf = 100000; // temporarily - to encode all data at once
 
-    // Setting the window
-    static String window = getWindow();
+    // Cursor position initialization
+    public static int pos = 0;
 
-    public void updateBuffer() {
-        // TODO
-    }
+    // Getting the data in the window (search forward)
+    public static String window = getWindow();
 
-    public static String getWindow() {
-        if (windowBuf > dataLen) {
-            return testData.substring(pos, dataLen);
+    // Getting the data in the search buffer (search backward)
+    public static String buffer = getBuffer();
+
+    // Method to process the data in the buffer (dealing with end cases)
+    public static String getBuffer() {
+        if (pos - searchBuf < 0) {
+            return testData.substring(0, pos);
         } else {
-            return testData.substring(pos, windowBuf);
+            return testData.substring(pos - searchBuf, pos);
         }
     }
-    
+
+    // Method to process the data in the window (dealing with end cases)
+    public static String getWindow() {
+        if (pos + windowBuf > dataLen) {
+            return testData.substring(pos, dataLen);
+        } else {
+            return testData.substring(pos, pos + windowBuf);
+        }
+    }
+
     // Method to find the indexes of substring matches in a string
     public static ArrayList<Integer> findMatches(String str, String subStr) {
         ArrayList<Integer> matches = new ArrayList<Integer>();
@@ -34,62 +47,83 @@ class LZJ4Encoder {
         return matches;
     }
 
-    public static ArrayList<Byte> createDataBlock(String symbols, int cursorPosition, int matchLength, ArrayList<Integer> matches) {
-        String hiToken = Integer.toHexString(matchLength);
-        String loToken = Integer.toHexString(matchLength-4);
-        int offset = cursorPosition - matchLength;
+    public static List<Byte> createDataBlock(String symbols, int cursorPosition, int matchLength,
+            ArrayList<Integer> matches) {
+        String hiToken = String.format("%04d", Integer.toBinaryString(matchLength));
+        String loToken = String.format("%04d", Integer.toBinaryString(matchLength - 1));
+        String token = hiToken + loToken;
 
-        ArrayList<Byte> dataBlock = new ArrayList<>();
+        // offset is the current buffer length minus the index of a match
+        int offset = buffer.length() - matches.get(0); // First element in matches
 
+        System.out.println("pos: " + cursorPosition);
+        System.out.println("symbols: " + symbols);
+        System.out.println("hi: " + hiToken);
+        System.out.println("lo: " + loToken);
+        System.out.println("token: " + token);
+        System.out.println("offset: " + offset);
+
+        List<Byte> dataBlock = new ArrayList<Byte>();
+        // dataBlock.add();
+
+        // incrementing pos
+        pos = pos + matchLength;
         return dataBlock;
     }
 
     public static void dataTraverse(String window) {
 
-        String bestMatch = "", subStr = "";
-        int matchLen = 0;
+        while (pos < dataLen) {
+            // System.out.println("pos: " + pos);
 
-        while(pos<dataLen) {
-            window = getWindow();
+            String bestMatch = "";
+            String subStr = "";
+            int matchLen = 0;
 
-            System.out.println("pos: "+ pos);
-            System.out.println(window);
+            for (int c = 0; c < window.length(); c++) {
+                // System.out.println("pos: " + pos);
 
-            String curByte = Character.toString(testData.charAt(pos)); // Get the current byte from the window
-            subStr = subStr.concat(curByte); // Add the current byte to the subStr ArrayList
-
-            // Matches must be >= 4
-            if (subStr.length() < 5) {
-                pos++;
-                continue;
-            }
-            System.out.println("SubStr: " + subStr);
-            ArrayList<Integer> matches = findMatches(window, subStr);
-            System.out.println("Match indexes: " + matches);
-
-            // If there are no matches, reset the subStr variable and continue
-            if (matches.isEmpty()) {
-                subStr = "";
-                continue;
-            } else {
-
-                //while(subStr == )
-                // If the length of the current best match is less than the length of the
-                // current substring
-                matchLen = subStr.length();
-                if (bestMatch.length() < matchLen) {
-                    // Replace the best match
-                    bestMatch = subStr;
-                    createDataBlock(bestMatch, pos, matchLen, matches);
+                // Matches must be >= 4
+                if (c < 5) {
+                    pos++;
+                    continue;
                 }
-                subStr = "";
+
+                window = getWindow();
+                buffer = getBuffer();
+                System.out.println("window: " + window);
+                System.out.println("buffer: " + buffer);
+
+                subStr = window.substring(0, c); // get the current subscrting from the window
+                System.out.println("subStr: " + subStr);
+
+                ArrayList<Integer> matches = findMatches(buffer, subStr);
+                System.out.println("Matches at pos: " + matches);
+
+                // If there are no matches, reset the subStr variable and continue
+                if (matches.isEmpty()) {
+                    subStr = "";
+                    pos++;
+                    continue;
+                } else {
+
+                    // If the length of the current best match is less than the length of the
+                    // current substring
+                    matchLen = subStr.length();
+                    if (bestMatch.length() < matchLen) {
+                        // Replace the best match
+                        bestMatch = subStr;
+                        createDataBlock(bestMatch, pos, matchLen, matches);
+                    }
+                    subStr = "";
+                }
 
             }
-            pos++;
+
+            // System.out.println("Best Match: " + bestMatch);
+            // System.out.println("Match length: " + matchLen);
+
         }
-        System.out.println("Window: " + window);
-        System.out.println("Best Match: " + bestMatch);
-        System.out.println("Match length: " + matchLen);
 
     }
 
